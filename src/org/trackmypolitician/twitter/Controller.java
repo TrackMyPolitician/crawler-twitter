3d98ae1f964c4fea138fc2335f5f8f081c1e9018
Baljit Singh <basingh89@gmail.com>
2017-03-12 03:01:01 -0400

    Accept authentication arguments from environment variables

--- src/org/trackmypolitician/twitter/Controller.java
+++ src/org/trackmypolitician/twitter/Controller.java
@@ -14,21 +14,44 @@ public final class Controller {
 	private static final Logger logger = LoggerFactory.getLogger(Controller.class);
 
 	/**
-	 * Parses command line arguments, and runs the crawler
+	 * Parses environment variables and/or command line arguments, and runs the
+	 * crawler
 	 * 
 	 * @param args
 	 *            Command line arguments
 	 */
 	public static void main(String[] args) {
 		try {
-			// Check input arguments
-			if (args.length < 2)
-				throw new Exception("Expecting 3 arguments: [Key] [Secret]");
-			else if (args.length > 2)
-				logger.info("Ignoring extra arguments");
+			Client twitter;
 
-			// Setup
-			Client twitter = new Client(args[0], args[1]);
+			if (args.length == 0) {
+				// Look for authentication in the ENV
+				String key = "CRAWLER_TWITTER_KEY", secret = "CRAWLER_TWITTER_SECRET";
+
+				if (System.getenv(key) == null)
+					throw new Exception("Missing environment variable: " + key);
+
+				if (System.getenv(secret) == null)
+					throw new Exception("Missing environment variable: " + secret);
+
+				twitter = new Client(System.getenv(key), System.getenv(secret));
+
+			} else if (args.length < 2)
+				throw new Exception("Expecting 2 arguments: [Key] [Secret]");
+
+			else {
+				// Authentication from command line arguments
+				twitter = new Client(args[0], args[1]);
+
+				// Obscure sensitive data
+				args[0] = null;
+				args[1] = null;
+
+				if (args.length > 2)
+					logger.info("Ignoring extra arguments");
+			}
+
+			// Setup crawler
 			Crawler crawler = new Crawler(twitter);
 
 			// Crawl indefinitely
